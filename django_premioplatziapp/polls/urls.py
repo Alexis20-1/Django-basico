8bc62b5c50bf4b99a0ac4dbd319c69a4a455bb38
alexis <alexis19012003@gmail.com>
2024-04-25 11:47:51 -0600

    agregando class view para buena practica y mejorando la logica

--- django_premioplatziapp/polls/urls.py
+++ django_premioplatziapp/polls/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path
-
 from . import views
-
-app_name = 'polls'
-
+app_name = "polls"
 urlpatterns = [
     #ex: /polls/
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     #ex: /polls/
-    path("<int:question_id>/detail/estaeslamejorpagina", views.detail, name="detail"),
+    path("<int:pk>/detail/estaeslamejorpagina", views.DetailView.as_view(), name="detail"),
     #ex: /polls/5/results
-    path("int:question_id>/results/", views.results, name="index"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     #ex: /polls/5/vote
-    path("int:question_id>/vote/", views.vote, name="index")
+    path("<int:question_id>/vote/", views.vote, name="vote")
 ]
